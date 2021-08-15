4c2263d8e730d86be9096b3473d0e0961b49b2c5
Dipang <Shethdipang@gmail.com>
2021-08-15 13:52:12 +0530

    add comments to know the use of header.swift file

--- IntentGitdemo/Support/Header/Header.swift
+++ IntentGitdemo/Support/Header/Header.swift
@@ -5,6 +5,10 @@
 //  Created by Dipang Sheth on 8/14/21.
 //
 
+/*
+ The header is struct where constant keywords, URLs, methods can be available which is useful in the lifetime of the app
+ */
+
 import Foundation
 
 struct Header {
